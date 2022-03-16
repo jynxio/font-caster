67909631deb05ae97615b0080f51e48a62764ae9
Matrix <1337816495@qq.com>
2022-03-17 00:17:29 +0800

    u

--- script/deduplication.js
+++ script/deduplication.js
@@ -1,11 +1,25 @@
 /**
- * 对unicode数组或字符串进行去重。
- * @param { string | Array<number> } data - 字符串（如"你我"）或存储unicode编码的数组（如[0x4f60, 0x6211]）。
+ * （内部方法）对unicode数组或字符串进行去重。
+ * @param { string | Array<number> } data - 字符串（如"ABC"）或存储unicode编码的数组（如[65, 66, 67]，采用十进制）。
  * @param { number } format - 输出值的格式，数字1代表字符串，数字2代表unicode数组。
  */
 function deduplication( data, format ) {
 
-    let data_format;
+    const unicodes = new Set();
+
+    for ( let item of data ) {
+
+        unicodes.add( typeof data === "number" ? item : item.codePointAt( 0 ) );
+
+    }
+
+    if ( format === 2 ) return Array.from( unicodes );
+
+    return Array.from( unicodes ).reduce( ( previous_value, current_value ) => {
+
+        return previous_value + String.fromCodePoint( current_value );
+
+    }, "" );
 
 }
 
