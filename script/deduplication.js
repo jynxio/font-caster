a6cb2cfbbcc9ca823ce0882da3f4f27457093f43
Matrix <1337816495@qq.com>
2022-03-16 23:49:06 +0800

    u

---
+++ script/deduplication.js
@@ -0,0 +1,12 @@
+/**
+ * 对unicode数组或字符串进行去重。
+ * @param { string | Array<number> } data - 字符串（如"你我"）或存储unicode编码的数组（如[0x4f60, 0x6211]）。
+ * @param { number } format - 输出值的格式，数字1代表字符串，数字2代表unicode数组。
+ */
+function deduplication( data, format ) {
+
+    let data_format;
+
+}
+
+module.exports = deduplication;
---
+++ script/subset.js
@@ -0,0 +1,14 @@
+/**
+ * 根据字符串或unicode数组来子集化字体文件。
+ * @param { string | Array<number> } data - 字符串（如"你我"）或存储unicode编码的数组（如[0x4f60, 0x6211]）。
+ * @param { string } path_origin - 原始的字体文件的路径，比如"./origin.otf"。
+ * @param { string } path_subset - 生成的字体文件的路径，比如"./sunset.otf"。
+ * @returns { undefined }
+ */
+function subset( data, path_origin, path_subset ) {
+
+    // TODO 先写去重
+
+}
+
+module.exports = subset;
